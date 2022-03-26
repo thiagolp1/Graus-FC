1772194b40ee223b7f9216b06295df9170efc262
Thiago <Thiagolopespereiralp1@gmail.com>
2022-03-25 21:34:24 -0300

    finalização do exercicio

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿double tempf, tempc;
+
+Console.Write("Digite a temperatura em graus Fahrenheit:");
+tempf = Convert.ToDouble(Console.ReadLine());
+
+tempc = (tempf - 32) / 1.8;
+
+
+Console.WriteLine($"{tempf}°F equivalem a {tempc:N2}°C");
